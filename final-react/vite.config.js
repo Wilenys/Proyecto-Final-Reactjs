b46edebc98c89d52680d7e473a09bb5182f23a26
Wilenys <andrea_9404@hotmail.com>
2025-12-06 17:07:19 -0300

    se modifico vite conf

--- final-react/vite.config.js
+++ final-react/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto-Final-React/', // ← IMPORTANTE
+  base: '/Proyecto-Final-Reactjs/', // ← IMPORTANTE
 })
